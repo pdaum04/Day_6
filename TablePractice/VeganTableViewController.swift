9a5ffa7d8f9fea56cbb2f52be2badf5ecfc33334
Apple <apple@Apples-MacBook-Pro-89.local>
2019-07-29 08:29:52 -0700

    End of Iteration 2

--- TablePractice/VeganTableViewController.swift
+++ TablePractice/VeganTableViewController.swift
@@ -8,27 +8,48 @@
 
 import UIKit
 
+struct cacti {
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+}
+
 class VeganTableViewController: UITableViewController {
+    var allMusic = [
+        cacti(id: 1,
+              title: "Kacey Musgraves",
+            text: "Kacey Musgraves is a five time Grammy Award winning Artist.",
+            image: "Kacey Country Gal"),
+        cacti(id: 2,
+              title: "Stevie Nicks",
+            text: "Stevie Nicks is a former member of the rock band Fleetwood Mac.",
+            image: "Stevie being a queen"),
+        cacti(id: 3,
+              title: "Taylor Swift",
+              text: "Taylor Swift is the daughter of Scott and Andrea Swift",
+              image: "Taylor with Benjamin Button")
+    ]
+    
 
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return allMusic.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-
+        cell.textLabel?.text = allMusic[indexPath.row].title
         return cell
     }
     
@@ -36,49 +57,5 @@ class VeganTableViewController: UITableViewController {
         return "Section \(section)"
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
